9d6854e838db9d9b507f07befcd1d7edcacae1b7
DuncanBScruggs <duncanbscruggs@gmail.com>
2020-10-08 15:48:53 -0400

    added logic for makingm0 buttoncol-6

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ class Calc{
     constructor(){
         // array of icons to use for buttons icon and id
         this.iconArr = ["AC","+/-","%","/","7","8","9","*","4","5","6","-","1","2","3","+","0",".","="]; 
-        this.numArr = [0];
+        this.numArr = ["7","8","9","4","5","6","1","2","3","0"];
+        this.mathArr = [0,0];
     }
 
     view(){
@@ -19,14 +20,14 @@ class Calc{
         // creates text field
         let txtField = document.createElement("div");
         txtField.setAttribute("class","col-12 text-right border border-dark");
-        txtField.innerHTML = this.numArr[0];
+        txtField.innerHTML = this.mathArr[0];
 
         // appends txtfield
         row.appendChild(txtField);
 
         // loops through icons to create buttons
         for (var i = 0; i < this.iconArr.length; i++) {
-            console.log(i);
+            // console.log(i);
             let bb = new makeButton(this.iconArr[i]);
             bb.bake();
             row.appendChild(bb.element);
@@ -36,10 +37,9 @@ class Calc{
         container.appendChild(row);
         main.appendChild(container);
 
-
-
+        
+        
     }
-
 }
 
 // make a button
@@ -47,25 +47,31 @@ class makeButton{
     constructor(icon){
         this.icon = icon;
         this.element = null;
-        this.doSomething = this.doSomething.bind(this);
+        this.doMath = this.doMath.bind(this);
     }
 
-    // need a method for operators and one for numbers
     // creates a button
     bake(){
         let col = document.createElement("div");
-        col.setAttribute("class", "col-3 text-center border border-dark");
-        col.setAttribute("id", this.icon);
+        
+        if (this.icon == "0"){
+            col.setAttribute("class", "col-6 text-center border border-dark");
+        }
+        else{
+            col.setAttribute("class", "col-3 text-center border border-dark");
+        }
         //console.log(this.icon);
+        col.addEventListener("click", this.doMath);
+        col.setAttribute("id", this.icon);
         let p = document.createElement("P")
         p.innerHTML = this.icon;
         col.appendChild(p);
-        col.addEventListener("click", this.doSomething);
+
         this.element = col;
     }
 
     // check to see if button is correctly targeted
-    doSomething(){
+    doMath(){
         console.log("pressed: " + this.icon);
     }
 } 
