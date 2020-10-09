925c5e16e02c73179d03fa506ef4e4fbe59b4250
DuncanBScruggs <duncanbscruggs@gmail.com>
2020-10-09 10:24:09 -0400

    basic working calculator with hard clear function

--- js/main.js
+++ js/main.js
@@ -4,8 +4,13 @@ class Calc{
         // array of icons to use for buttons icon and id
         this.iconArr = ["AC","+/-","%","/","7","8","9","*","4","5","6","-","1","2","3","+","0",".","="]; 
         // this.numArr = ["7","8","9","4","5","6","1","2","3","0"];
-        this.mathArr = [0,0];
-        this.parent = this.parent.bind(this);
+        this.txtField = document.createElement("div");
+        this.mathArr = ["",""];
+        this.opp = "";
+        this.oppUsed = false;
+        this.calculate = this.calculate.bind(this);
+        this.getNum = this.getNum.bind(this);
+        this.mathTime = this.mathTime.bind(this);
     }
 
     view(){
@@ -19,17 +24,17 @@ class Calc{
         row.setAttribute("class", "row");
 
         // creates text field
-        let txtField = document.createElement("div");
-        txtField.setAttribute("class","col-12 text-right border border-dark");
-        txtField.innerHTML = this.mathArr[0];
+        
+        this.txtField.setAttribute("class","col-12 text-right border border-dark");
+        this.txtField.innerHTML = "0";
 
         // appends txtfield
-        row.appendChild(txtField);
+        row.appendChild(this.txtField);
 
         // loops through icons to create buttons
         for (var i = 0; i < this.iconArr.length; i++) {
             // console.log(i);
-            let bb = new makeButton(this.iconArr[i], this.parent);
+            let bb = new makeButton(this.iconArr[i], this.getNum, this.mathTime);
             bb.bake();
             row.appendChild(bb.element);
         }
@@ -40,19 +45,85 @@ class Calc{
 
 
     }
-    parent(x){
-        console.log("YES", x);
+    getNum(x){
+        console.log(x);
+
+        if(this.oppUsed == false){
+            console.log(x);
+            this.mathArr[0] += x;
+            console.log(this.mathArr)
+            this.txtField.innerHTML = this.mathArr[0];
+        }
+        else if (x == "="){
+            // console.log(x);
+            this.calculate();
+            console.log(this.mathArr);
+        }
+        else{
+            console.log(x);
+            this.mathArr[1] += x;
+            console.log(this.mathArr)
+            this.txtField.innerHTML = this.mathArr[1];
+        }
+
+    }
+
+    mathTime(x){
+        if(x == "AC"){
+            this.mathArr[0] = "";
+            this.mathArr[1] = "";
+            this.txtField.innerHTML = "0";
+            this.oppUsed = false;
+            this.opp = "";
+        }
+        else{
+            this.oppUsed = true;
+            this.opp = x;
+            console.log(this.opp);
+        }
+    }
+
+    calculate(){
+        if(this.opp == "+"){
+            let j = Number.parseInt(this.mathArr[0])
+            let y = Number.parseInt(this.mathArr[1]);
+            j += y;
+            this.mathArr[0] = j.toString();
+            this.txtField.innerHTML = this.mathArr[0];
+        }
+        if(this.opp == "-"){
+            let j = Number.parseInt(this.mathArr[0])
+            let y = Number.parseInt(this.mathArr[1]);
+            j -= y;
+            this.mathArr[0] = j.toString();
+            this.txtField.innerHTML = this.mathArr[0];
+        }
+        if(this.opp == "*"){
+            let j = Number.parseInt(this.mathArr[0])
+            let y = Number.parseInt(this.mathArr[1]);
+            j *= y;
+            this.mathArr[0] = j.toString();
+            this.txtField.innerHTML = this.mathArr[0];
+        }
+        if(this.opp == "/"){
+            let j = Number.parseInt(this.mathArr[0])
+            let y = Number.parseInt(this.mathArr[1]);
+            j /= y;
+            this.mathArr[0] = j.toString();
+            this.txtField.innerHTML = this.mathArr[0];
+        }
     }
 }
 
 // make a button
 class makeButton{
-    constructor(icon, method){
+    constructor(icon, getNum, mathTime){
         this.icon = icon;
         this.element = null;
         this.doMath = this.doMath.bind(this);
-        // this.mathArr = mathArr;
-        this.method = method
+        this.giveNum = this.giveNum.bind(this);
+        this.getNum = getNum;
+        this.mathTime = mathTime;
     }
 
     // creates a button
@@ -60,13 +131,24 @@ class makeButton{
         let col = document.createElement("div");
         
         if (this.icon == "0"){
-            col.setAttribute("class", "col-6 text-center border border-dark");
+            col.setAttribute("class", "col-6 number text-center border border-dark");
+            col.addEventListener("click", this.giveNum);
+        }
+        else if (this.icon == "1"||this.icon == "2"||this.icon == "3"||this.icon == "4"||
+            this.icon == "5"||this.icon == "6"||this.icon == "7"||this.icon == "8"||this.icon == "9"){
+            col.setAttribute("class", "col-3 number text-center border border-dark");
+            col.addEventListener("click", this.giveNum);
+        }
+        else if (this.icon == "="){
+            col.setAttribute("class", "col-3 operator text-center border border-dark");
+            col.addEventListener("click", this.giveNum);
         }
         else{
-            col.setAttribute("class", "col-3 text-center border border-dark");
+            col.setAttribute("class", "col-3 operator text-center border border-dark");
+            col.addEventListener("click", this.doMath);
         }
         //console.log(this.icon);
-        col.addEventListener("click", this.doMath);
+        
         col.setAttribute("id", this.icon);
         let p = document.createElement("P")
         p.innerHTML = this.icon;
@@ -75,11 +157,14 @@ class makeButton{
         this.element = col;
     }
 
+    giveNum(){
+        this.getNum(this.icon);
+    }
+
     // check to see if button is correctly targeted
     doMath(){
         // console.log("pressed: " + this.icon);
-        this.method(this.icon);
-
+        this.mathTime(this.icon)
     }
 } 
 
